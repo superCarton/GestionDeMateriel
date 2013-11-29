9ef7b5f125a5f95880d6b390b83de3be71627961
Tom GUILLERMIN <tom.guillermin@gmail.com>
2013-11-29 10:54:40 +0100

    Added Getters and Setters for User

--- src/borrowmanager/user/User.java
+++ src/borrowmanager/user/User.java
@@ -22,4 +22,48 @@ public class User {
 	public Integer getMaxReservationLength() {
 		return UserType.getMaxReservationLength(userType);
 	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the userType
+	 */
+	public UserType getUserType() {
+		return userType;
+	}
+
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+	
+	
 }
