f86c59c7023b6c08d026eade048b8b23cd88fb4b
Tom GUILLERMIN <tom.guillermin@gmail.com>
2013-11-29 11:29:47 +0100

    Implemented the Manager.

--- src/borrowmanager/Manager.java
+++ src/borrowmanager/Manager.java
@@ -1,10 +1,13 @@
 package borrowmanager;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import borrowmanager.booking.BookingCalendar;
+import borrowmanager.booking.DateInterval;
 import borrowmanager.user.User;
 import borrowmanager.element.BorrowableG;
 import borrowmanager.booking.Booking;
@@ -15,34 +18,98 @@ public class Manager {
 	private List<BorrowableG> stock;
 
 	public Manager(User user) {
-		throw new UnsupportedOperationException();
+		this.currentUser = user;
 	}
 
-	public Boolean book(Integer borrowableId, Integer borrowerId, Date start, Date end) {
-		throw new UnsupportedOperationException();
+	public Boolean book(Integer borrowableId, Integer borrowerId, Date start, Date end, String reason) {
+		// Date verifications
+		
+		// Time before the beggining of the booking (reservation)
+		DateInterval reservationStartInterval = new DateInterval(new Date(), start);
+		if (reservationStartInterval.getLength() > currentUser.getMaxReservationLength()) {
+			return false;
+		}
+		
+		// Duration of the booking
+		DateInterval bookingInterval= new DateInterval(start, end);
+		if (bookingInterval.getLength() > currentUser.getMaxBookingLength()) {
+			return false;
+		}
+		
+		BookingCalendar calendar = bookings.get(borrowerId);
+		
+		if (calendar == null) {
+			return false;
+		}
+		
+		return calendar.book(borrowerId, currentUser.getId(), bookingInterval, reason);
 	}
 
 	public Boolean isAvailable(Integer borrowableId) {
-		throw new UnsupportedOperationException();
+		BorrowableG b = getBorrowableById(borrowableId);
+		if (b != null) {
+			Date now = new Date();
+			return isAvailable(borrowableId, now, now);
+		}
+		return false;
 	}
 
-	public void isAvailable(Integer borrowableId, Date start, Date end) {
-		throw new UnsupportedOperationException();
+	public Boolean isAvailable(Integer borrowableId, Date start, Date end) {
+		BookingCalendar calendar = bookings.get(borrowableId);
+		return calendar.isAvailable(start, end);
 	}
 
 	public Boolean giveBack(Integer borrowableId) {
-		throw new UnsupportedOperationException();
+		BookingCalendar calendar = bookings.get(borrowableId);
+		if (calendar == null) {
+			return false;
+		}
+		
+		Booking b = calendar.getCurrentBooking(); 
+		if (b == null) {
+			return false;
+		}
+		
+		// TODO : end() returns false if the borrowable was given back too late.
+		return b.end();
+		
 	}
 
 	public List<Booking> getNotYetValidatedBookings() {
-		throw new UnsupportedOperationException();
+		List<Booking> notValidatedBookings = new LinkedList<Booking>();
+		for (BookingCalendar calendar : bookings.values()) {
+			for(Booking booking : calendar.getBookings()) {
+				if (!booking.isValidated()) {
+					notValidatedBookings.add(booking);
+				}
+			}
+		}
+		return notValidatedBookings;
+		
 	}
 
 	public Map<Integer, String> getStockDescriptionForFeature(String feature) {
-		throw new UnsupportedOperationException();
+		Map<Integer, String> descriptions = new HashMap<Integer, String>();
+		for (BorrowableG borrowable : stock) {
+			if (borrowable.hasFeature(feature)) {
+				descriptions.put(borrowable.getId(), borrowable.getName());
+			}
+		}
+		return descriptions;
 	}
 
-	public void getFullDescription(Integer id) {
-		throw new UnsupportedOperationException();
+	public BorrowableG getBorrowableById(Integer id) {
+		for (BorrowableG borrowable : stock) {
+			if (borrowable.getId() == id) {
+				return borrowable;
+			}
+		}
+		return null;
+	}
+	
+	public String getFullDescription(Integer id) {
+		BorrowableG borrowable = getBorrowableById(id);
+		// TODO : more data
+		return borrowable.getName();
 	}
 }
